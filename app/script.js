ec2c6492e3b6bf7a001b81177c576a7e889e213b
Mohamed-Magdy-fayed <mohamed.ahmed.sc2@gmail.com>
2021-09-24 17:07:08 +0200

    responsive top bar was made

--- app/script.js
+++ app/script.js
@@ -6,13 +6,27 @@ const open = () => {
     nav.classList.add('list')
 }
 
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 880) {
+        nav.style.display = 'none'
+        hamburger.classList.remove('opened')
+        nav.classList.add('list')
+    } else {
+        nav.style.display = 'flex'
+        hamburger.classList.add('opened')
+        nav.classList.remove('list')
+    }
+})
+
 hamburger.addEventListener('click', () => {
     if (hamburger.classList.contains('opened')) {
         hamburger.classList.remove('opened')
         nav.classList.remove('list')
+        nav.style.display = 'none'
     } else {
         hamburger.classList.add('opened')
         nav.classList.add('list')
+        nav.style.display = 'flex'
         }
 
 })
